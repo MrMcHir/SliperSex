056e9b65a6e5a9ff72d5f9a8b6a56e8a30cbf961
MrMcHir <102164593+MrMcHir@users.noreply.github.com>
2022-07-17 13:38:23 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 263761432 then
-  -- soon Horrfic
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/MrMcHir/SliperSex/main/Horrific%20Housing.lua'))()
 end
 
 if game.PlaceId == 1962086868 then
-  -- soon Horrfic
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/MrMcHir/SliperSex/main/TOH.lua'))()
 end
